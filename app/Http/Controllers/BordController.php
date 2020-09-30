4486ba7a18c0a9390d95f475e068086acf9ddfa2
shkireal <shkkireal@gmail.com>
2020-09-30 12:19:40 +0300

    debug

--- app/Http/Controllers/BordController.php
+++ app/Http/Controllers/BordController.php
@@ -6,10 +6,12 @@ use Illuminate\Http\Request;
 
 class BordController extends Controller
 {
-    public function index(){
+     public $last;
+
+    public function index($last){
 
 
         Redis::get('post', $this->last);
-        return view::welcome($this->last);
+        return view('welcome', $data=[$last]);
     }
 }
