a115b43178f82f3f6d6a6727a1c2a591c5873439
shkireal <shkkireal@gmail.com>
2020-10-02 11:51:57 +0300

    2

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/bot', 'BordController@updatetelegrame');
 
 
-Route::get('/', 'HomeController@index' );
+Route::get('/', 'HomeController@index' )->name('home');
+Route::get('/signup', 'AuthController@GetSignup')->name('auth.signup');
+Route::post('/', 'AuthController@PostSignup');
